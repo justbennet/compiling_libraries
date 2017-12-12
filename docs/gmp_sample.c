b2abd891ec7bb10b7227eb63960c3d3dc1d5c943
justbennet <justbennet@users.noreply.github.com>
2017-12-11 21:41:36 -0500

    Create gmp_sample.c

---
+++ docs/gmp_sample.c
@@ -0,0 +1,45 @@
+/*  Example from Tutorial on GMP
+    https://www.cs.colorado.edu/~srirams/courses/csci2824-spr14/gmpTutorial.html
+    Copied on Mon Dec 11 19:49:12 EST 2017
+
+    Factorial using arbitrary precision
+*/
+
+#include "gmp.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void fact(int n){
+  int i;
+  mpz_t p;
+
+  mpz_init_set_ui(p,1); /* p = 1 */
+  for (i=1; i <= n ; ++i){
+    mpz_mul_ui(p,p,i); /* p = p * i */
+  }
+  printf ("%d!  =  ", n);
+  mpz_out_str(stdout,10,p);
+  mpz_clear(p);
+
+}
+
+int main(int argc, char * argv[]){
+  int n;
+
+
+  if (argc <= 1){
+    printf ("Usage: %s <number> \n", argv[0]);
+    return 2;
+  }
+
+  n = atoi(argv[1]);
+  assert( n >= 0);
+  fact(n);
+  printf("\n");
+
+/* The sample originally returned 1 on success, which is
+   bogus.
+*/
+  return 0;
+}
